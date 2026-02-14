eea215f2f8c6adaed45ec6537aa30ba516a3ebe3
白影White <86943191+bywhite0@users.noreply.github.com>
2026-02-14 15:36:25 +0800

    chore: using OneBot V11

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import os
 
 import nonebot
 from nonebot.adapters.console import Adapter as ConsoleAdapter
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 from src.core.services.dm_provider import init_dm
 
 
 nonebot.init()
 driver = nonebot.get_driver()
-driver.register_adapter(ConsoleAdapter)
+# driver.register_adapter(ConsoleAdapter)
+driver.register_adapter(ONEBOT_V11Adapter)
 nonebot.load_plugin("nonebot_plugin_localstore")
 plugin_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "src", "plugins", "llll"))
 nonebot.load_plugins(plugin_dir)
